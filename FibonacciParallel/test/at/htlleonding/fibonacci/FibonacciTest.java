1077d7680aadcff652a850da3264de99681fab30
Peter Bauer <peter.bauer@bajupa.com>
2024-02-14 16:01:32 +0100

    Minor adaptations after migrating to new Java version

--- FibonacciParallel/test/at/htlleonding/fibonacci/FibonacciTest.java
+++ FibonacciParallel/test/at/htlleonding/fibonacci/FibonacciTest.java
@@ -9,12 +9,12 @@
  * copies, modifications, and distributions. Contact the Head of Informatics,
  * Media Technique and Design, HTBLA Leonding, Limesstr. 12 - 14, 4060 Leonding,
  * Austria, for commercial licensing opportunities.
- * 
+ *
  * IN NO EVENT SHALL HTBLA LEONDING BE  LIABLE TO ANY PARTY FOR DIRECT,
  * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST
  * PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION,
  * EVEN IF HTBLA LEONDING HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * HTBLA LEONDING SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
  * PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
@@ -23,49 +23,48 @@
  */
 package at.htlleonding.fibonacci;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- *
  * @author P. Bauer <p.bauer@htl-leonding.ac.at>
  */
 public class FibonacciTest {
     private final int N = 45;
-    
+
+
     @Test
     public void testFirstNumber() {
-        
+
         assertEquals(1, Fibonacci.getNumberSingle(0));
         assertEquals(1, Fibonacci.getNumberSingle(1));
-        
+
         assertEquals(1, Fibonacci.getNumberParallel(0));
         assertEquals(1, Fibonacci.getNumberParallel(1));
     }
-    
+
     @Test
     public void testSecondNumber() {
         assertEquals(2, Fibonacci.getNumberSingle(2));
         assertEquals(2, Fibonacci.getNumberParallel(2));
     }
-    
+
     @Test
     public void testThirdNumber() {
         assertEquals(3, Fibonacci.getNumberSingle(3));
         assertEquals(3, Fibonacci.getNumberParallel(3));
     }
-    
+
     @Test
     public void testFourthNumber() {
         assertEquals(5, Fibonacci.getNumberSingle(4));
         assertEquals(5, Fibonacci.getNumberParallel(4));
     }
-    
+
     @Test
-    @Ignore
+    @Disabled
     public void testLarge() {
         assertEquals(1_836_311_903, Fibonacci.getNumberSingle(N));
         assertEquals(1_836_311_903, Fibonacci.getNumberParallel(N));
